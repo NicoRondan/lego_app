e8b43683c528c392cfb481ffa1bc63c0bd9b8967
nicolas <nicolas@toptive.co>
2025-08-24 20:20:12 -0300

    remove card name

--- ecommerce-frontend/src/components/home/CategoryCarousel.jsx
+++ ecommerce-frontend/src/components/home/CategoryCarousel.jsx
@@ -41,9 +41,6 @@ function CategoryCarousel() {
                         className="d-block w-100 rounded"
                         loading="lazy"
                       />
-                      <span className="position-absolute top-50 start-50 translate-middle text-white fw-bold">
-                        {cat.name}
-                      </span>
                     </div>
                   </Link>
                 </div>
